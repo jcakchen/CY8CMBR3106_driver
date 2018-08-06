ba2fec0bb6d80d792e9703362d72a32a70972504
Jack Chen <jackchenqust@gmail.com>
2018-08-06 15:45:21 +0800

    debug

--- MBR3_Int_BTN_Status.py
+++ MBR3_Int_BTN_Status.py
@@ -6,110 +6,13 @@
 import sys
 import time
 import smbus
+from _button import Button
 
-
-#------------------------------------------------------------#
-#/* CY8CMBR3116 Register Map Offset Address */               #
-#REGMAP_ORIGIN			= 0x00
-#SENSOR_PATTERN			= 0x00
-#FSS_EN					= 0x02
-#TOGGLE_EN				= 0x04
-#LED_ON_EN				= 0x06
-#SENSITIVITY0			= 0x08
-#SENSITIVITY1			= 0x09
-#SENSITIVITY2			= 0x0A
-#SENSITIVITY3			= 0x0B
-#BASE_THRESHOLD0		= 	0x0C
-#BASE_THRESHOLD1		= 	0x0D
-#FINGER_THRESHOLD2		= 0x0E
-#FINGER_THRESHOLD3		= 0x0F
-#FINGER_THRESHOLD4		= 0x10
-#FINGER_THRESHOLD5		= 0x11
-#FINGER_THRESHOLD6		= 0x12
-#FINGER_THRESHOLD7		= 0x13
-#FINGER_THRESHOLD8		= 0x14
-#FINGER_THRESHOLD9		= 0x15
-#FINGER_THRESHOLD10		= 0x16
-#FINGER_THRESHOLD11		= 0x17
-#FINGER_THRESHOLD12		= 0x18
-#FINGER_THRESHOLD13		= 0x19
-#FINGER_THRESHOLD14		= 0x1A
-#FINGER_THRESHOLD15		= 0x1B
-#SENSOR_DEBOUNCE		= 	0x1C
-#BUTTON_HYS				= 0x1D
-#BUTTON_BUT				= 0x1E
-#BUTTON_LBR				= 0x1F
-#BUTTON_NNT				= 0x20
-#BUTTON_NT				= 0x21
-#PROX_EN				= 	0x26
-#PROX_CFG				= 0x27
-#PROX_CFG2				= 0x28
-#PROX_TOUCH_TH0			= 0x2A
-#PROX_TOUCH_TH1			= 0x2C
-#PROX_HYS				= 0x30
-#PROX_BUT				= 0x31
-#PROX_LBR				= 0x32
-#PROX_NNT				= 0x33
-#PROX_NT				= 	0x34
-#PROX_POSITIVE_TH0		= 0x35
-#PROX_POSITIVE_TH1		= 0x36
-#PROX_NEGATIVE_TH0		= 0x39
-#PROX_NEGATIVE_TH1		= 0x3A
-#LED_ON_TIME			= 	0x3D
-#BUZZER_CFG				= 0x3E
-#BUZZER_ON_TIME			= 0x3F
-#GPO_CFG				= 	0x40
-#PWM_DUTYCYCLE_CFG0		= 0x41
-#PWM_DUTYCYCLE_CFG1		= 0x42
-#PWM_DUTYCYCLE_CFG2		= 0x43
-#PWM_DUTYCYCLE_CFG3		= 0x44
-#PWM_DUTYCYCLE_CFG4		= 0x45
-#PWM_DUTYCYCLE_CFG5		= 0x46
-#PWM_DUTYCYCLE_CFG6		= 0x47
-#PWM_DUTYCYCLE_CFG7		= 0x48
-#SPO_CFG				= 	0x4C
-#DEVICE_CFG0			= 	0x4D
-#DEVICE_CFG1			= 	0x4E
-#DEVICE_CFG2			= 	0x4F
-#I2C_ADDR				= 0x51
-#REFRESH_CTRL			= 0x52
-#STATE_TIMEOUT			= 0x55
-#SLIDER_CFG				= 0x5D
-#SLIDER1_CFG			= 	0x61
-#SLIDER1_RESOLUTION		= 0x62
-#SLIDER1_THRESHOLD		= 0x63
-#SLIDER2_CFG			= 	0x67
-#SLIDER2_RESOLUTION		= 0x68
-#SLIDER2_THRESHOLD		= 0x69
-#SLIDER_DEBOUNCE		= 	0x6F
-#SLIDER_BUT				= 0x70
-#SLIDER_LBR				= 0x71
-#SLIDER_NNT				= 0x72
-#SLIDER_NT				= 0x73
-#CONFIG_CRC				= 0x7E
-#GPO_OUTPUT_STATE		= 0x80
-#SENSOR_ID				= 0x82
-#CTRL_CMD				= 0x86
-#BUTTON_STATUS			= 0xAA
-
-#/* Command Codes */
-#CMD_NULL				= 0x00
-#SAVE_CHECK_CRC         =  0x02
-#CALC_CRC               =  0x03
-#LOAD_FACTORY           =  0x04
-#LOAD_PRIMARY           =  0x05
-#LOAD_SECONDARY         =  0x06
-#SLEEP                  =  0x07
-#CLEAR_LATCHED_STATUS   =  0x08
-#CMD_RESET_PROX0_FILTER	= 0x09
-#CMD_RESET_PROX1_FILTER	= 0x0A
-#ENTER_CONFIG_MODE      =  0x0B
-#EXIT_CONTROL_RUN       =  0xFE
-#SW_RESET               =  0xFF
-
+# GPIO definitions (BCM)
+GPIO_BUTTON = 23
+gpio_button_int = Button(channel=GPIO_BUTTON)
 
 bus = smbus.SMBus(1)
-
 #/* Slave Address (Default) */
 SLAVE_ADDR = 0x37
 
@@ -285,8 +188,17 @@ def displayButtonStat():
         print('Exiting th Program')
         stop = 1
     return
-   
-  
+
+def on_button_pressed():
+    slider1Position = bus.read_byte_data(SLAVE_ADDR, SILIDER1_POSITION)
+    print('slider1Position %d' % slider1Position)
+    slider2Position = bus.read_byte_data(SLAVE_ADDR, SILIDER2_POSITION)
+    print('slider2Position %d' % slider2Position)	
+    buttonStat = bus.read_byte_data(SLAVE_ADDR, BTN_STAT)
+    print('buttonStat %d ' % buttonStat)
+    addr = bus.read_byte_data(SLAVE_ADDR, 0x90)
+    print(addr)      
+
 if __name__ == "__main__":
     global stop
     global slider1Position
@@ -298,12 +210,14 @@ if __name__ == "__main__":
     
     #global flag to stop the thread
     stop = 0 
+    gpio_button_int.on_press(on_button_pressed)  
     init_MBR3()
     while 1:
         retry = 1
         time.sleep(0.2) 
         while(retry):
             try:
+                """
                 slider1Position = bus.read_byte_data(SLAVE_ADDR, SILIDER1_POSITION)
                 print('slider1Position %d' % slider1Position)
                 slider2Position = bus.read_byte_data(SLAVE_ADDR, SILIDER2_POSITION)
@@ -311,7 +225,8 @@ if __name__ == "__main__":
                 buttonStat = bus.read_byte_data(SLAVE_ADDR, BTN_STAT)
                 print('buttonStat %d ' % buttonStat)
                 addr = bus.read_byte_data(SLAVE_ADDR, 0x90)
-                print(addr)                
+                print(addr)
+                """                
                 retry = 0
    
             except KeyboardInterrupt:
